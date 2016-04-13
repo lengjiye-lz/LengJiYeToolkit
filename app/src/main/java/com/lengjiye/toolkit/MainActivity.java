31a7556560b81674aba57fd0a03f55dea73bbb93
qiduzao <qiduzao@j1.com>
2016-04-13 14:41:39 +0800

    udpate

--- app/src/main/java/com/lengjiye/toolkit/MainActivity.java
+++ app/src/main/java/com/lengjiye/toolkit/MainActivity.java
@@ -3,13 +3,9 @@ package com.lengjiye.toolkit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.lengjiye.toolkit.activity.BaseActivity;
 import com.lengjiye.toolkit.activity.StretchTextActivity;
-import com.lengjiye.toolkit.view.Toast;
-
-import org.xutils.view.annotation.ViewInject;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
@@ -18,7 +14,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.stretch).setOnClickListener(this);
-        Toast.makeText(this, "asdc", Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/lengjiye/toolkit/activity/StretchTextActivity.java
+++ app/src/main/java/com/lengjiye/toolkit/activity/StretchTextActivity.java
@@ -1,11 +1,10 @@
 package com.lengjiye.toolkit.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.lengjiye.toolkit.R;
 
-public class StretchTextActivity extends AppCompatActivity {
+public class StretchTextActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
