975586f0965ae71dd728490f3c643a5b1bbb3a42
qiduzao <qiduzao@j1.com>
2016-07-20 17:09:24 +0800

    add tween animation

--- app/src/main/java/com/lengjiye/toolkit/fragment/TweenAnimationFragment.java
+++ app/src/main/java/com/lengjiye/toolkit/fragment/TweenAnimationFragment.java
@@ -5,6 +5,14 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 import com.lengjiye.toolkit.R;
 
@@ -14,6 +22,7 @@ import com.lengjiye.toolkit.R;
  */
 public class TweenAnimationFragment extends BaseFragment {
 
+    private ImageView image1, image2, image3, image4, image5;
 
     public TweenAnimationFragment() {
     }
@@ -30,13 +39,58 @@ public class TweenAnimationFragment extends BaseFragment {
 
     @Override
     public View initView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_property_animation, container, false);
+        View view = inflater.inflate(R.layout.fragment_tween_animation, container, false);
+        image1 = (ImageView) view.findViewById(R.id.image1);
+        image2 = (ImageView) view.findViewById(R.id.image2);
+        image3 = (ImageView) view.findViewById(R.id.image3);
+        image4 = (ImageView) view.findViewById(R.id.image4);
+        image5 = (ImageView) view.findViewById(R.id.image5);
         return view;
     }
 
     @Override
     public void initData() {
         super.initData();
+        image1.setOnClickListener(this);
+        image2.setOnClickListener(this);
+        image3.setOnClickListener(this);
+        image4.setOnClickListener(this);
+        image5.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.image1:
+                Animation tween_scale = AnimationUtils.loadAnimation(mContext, R.anim.tween_scale);
+                image1.startAnimation(tween_scale);
+                break;
+            case R.id.image2:
+                Animation tween_rotate = AnimationUtils.loadAnimation(mContext, R.anim.tween_rotate);
+                image2.startAnimation(tween_rotate);
+                break;
+            case R.id.image3:
+                Animation tween_translate = AnimationUtils.loadAnimation(mContext, R.anim.tween_translate);
+                image3.startAnimation(tween_translate);
+                break;
+            case R.id.image4:
+                Animation tween_alpha = AnimationUtils.loadAnimation(mContext, R.anim.tween_alpha);
+                image4.startAnimation(tween_alpha);
+                break;
+            case R.id.image5:
+                AnimationSet animationSet = new AnimationSet(true);
+                ScaleAnimation scaleAnimation = new ScaleAnimation(0, 1, 0, 1, 0.5f, 0.5f);
+                RotateAnimation rotateAnimation = new RotateAnimation(0, 360, 0.5f, 0.5f);
+                TranslateAnimation translateAnimation = new TranslateAnimation(0, 200, 0, 0);
+                AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+                animationSet.addAnimation(scaleAnimation);
+                animationSet.addAnimation(rotateAnimation);
+                animationSet.addAnimation(translateAnimation);
+                animationSet.addAnimation(alphaAnimation);
+                animationSet.setDuration(3000);
+                image5.startAnimation(animationSet);
+                break;
+        }
     }
 }
